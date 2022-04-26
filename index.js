3623e76c7d36c11af978240f11264d03dbf4323a
Mathieu Noel <yzorien@gmail.com>
2022-04-26 22:44:48 +0200

    avec le bonus mon seigneur

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const readline = require('readline');
 const hitParade = [
     `Jain - Come`,
     `Matt Simons - Catch & Realease`,
-    `Twety One Pilots - Stressed Out`,
+    `Twenty One Pilots - Stressed Out`,
     `Justin Bieber - Love Yourself`,
     `Kids United - On écrit sur les murs`,
     `Rihanna - Work`,
@@ -15,30 +15,44 @@ const hitParade = [
 ];
 
 
-// Votre code va ici
+// Votre code va ici.
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
+  // Je récupère la value de mon array.
 const table= function(value) {
     for(value of hitParade) {
         console.log(value)
     }
 }
-const game = function() {
+// Je crée une fonction game() que je vais rappeler après chaque if et else if sauf celui qui rl.close(). 
+const game = function() {  
   rl.question(`Que souhaitez vous ?`, (answer) => {
+      // ## Bonus j'utilise un .split pour récuperer mon indice.
+      let position = answer.split(" ");
+        // Première question
       if(answer === `chante`) {
-          console.log(`Je m'appelle Charlu, je m'appelle Lili, vous êtes chez O'clock`);
-          game()
+        console.log(`Je m'appelle Charlu, je m'appelle Lili, vous êtes chez O'clock`);
+        game();
+        // Deuxième question
       } else if(answer === `classement`) {          
-          table();
-          game()             
+        table();
+        game(); 
+        // La sortie de fonction fléchée            
       } else if(answer === `quitter`) {
         rl.close();
+        // ## Bonus pour extraire l'indice demandé du array.
+      } else if(answer === "position " + position[1]) {       
+        console.log(hitParade[position[1]-1]);        
+        game();
+        // On repose la question si les réponces sont differantes
       } else {
-        game()
+        game();
       }      
-    })
+    }
+    )
 }
+// J'execute une fois la fonction pour lancer la machine.
 game()
